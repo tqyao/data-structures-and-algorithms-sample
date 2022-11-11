62899aab14450f029f09c3019456c1a225f96248
tqyao <tqyao@foxmail.com>
2022-11-11 22:27:43 +0800

    BinarySortTree_04 排序二叉树 1. 在指定的排序二叉树中查找指定节点的父节点 2. 删除节点 BinarySortTree_04Test 排序二叉树测试用例

--- data-structures-demo/src/main/java/fun/tqyao/tree/BinarySortTree_04.java
+++ data-structures-demo/src/main/java/fun/tqyao/tree/BinarySortTree_04.java
@@ -3,6 +3,8 @@
  */
 package fun.tqyao.tree;
 
+import java.util.*;
+
 /**
  * 二叉排序树
  *
@@ -13,6 +15,361 @@ package fun.tqyao.tree;
 public class BinarySortTree_04 {
 
     BinarySortNode root;
+
+
+    /**
+     * 中序 非递归
+     *
+     * @return java.util.List<java.lang.Integer>
+     * @author tqyao
+     * @create 2022/11/11 17:19
+     */
+    public List<Integer> inOrderTraverse() {
+        List<Integer> orderList = new ArrayList<>();
+        Stack<BinarySortNode> stack = new Stack<>();
+        BinarySortNode temp = root;
+        while (!stack.isEmpty() || temp != null) {
+            if (temp != null) {
+                stack.push(temp);
+                temp = temp.lchild;
+            } else {
+                // 第二次处理该节点
+                temp = stack.pop();
+                orderList.add(temp.data);
+                // 转向右子树
+                temp = temp.rchild;
+            }
+        }
+        return orderList;
+    }
+
+    /**
+     * 中序递归遍历
+     *
+     * @param root
+     * @param order
+     * @return java.util.List<java.lang.Integer>
+     * @author tqyao
+     * @create 2022/11/11 16:56
+     */
+    public List<Integer> inOrderRecursion(BinarySortNode root, List<Integer> order) {
+        if (order == null)
+            order = new ArrayList<>();
+        if (root == null)
+            return order;
+        inOrderRecursion(root.lchild, order);
+        order.add(root.data);
+        inOrderRecursion(root.rchild, order);
+        return order;
+    }
+
+
+    /**
+     * 删除节点
+     *
+     * @param value
+     * @return boolean
+     * @author tqyao
+     * @create 2022/11/11 20:24
+     */
+    public boolean remove(int value) {
+        // 指定值是否存在对应节点在排序树中
+        BinarySortNode delNode = searchNodeTraverse(value);
+        // 不存在返回删除失败
+        if (delNode == null)
+            return false;
+
+        BinarySortNode parent = searchParent(root, delNode);
+        // 1. 删除的是叶子节点
+        if (delNode.lchild == null && delNode.rchild == null) {
+            // 叶子节点就是根节点
+            if (parent == null)
+                root = null;
+            else if (parent.lchild == delNode)
+                // 删除节点是其父节点的左孩子
+                parent.lchild = null;
+            else
+                // 否则删除节点是其父节点的右孩子
+                parent.rchild = null;
+            return true;
+        }
+
+        // 2. 删除节点有且只有一个孩子
+        // 2.1 删除节点有左孩子没有有孩子
+        if (delNode.lchild != null && delNode.rchild == null) {
+            // 要删除的是只有左子树的根节点
+            if (parent == null)
+                root = delNode.lchild;
+            else if (parent.lchild == delNode)
+                // 删除节点是其父节点的左孩子
+                parent.lchild = delNode.lchild;
+            else
+                // 否则删除节点是其父节点的右孩子
+                parent.rchild = delNode.lchild;
+            return true;
+        }
+
+        // 2.2 删除节点无左孩子有右孩子
+        if (delNode.lchild == null && delNode.rchild != null) {
+            if (parent == null)
+                root = delNode.rchild;
+            else if (parent.lchild == delNode)
+                parent.lchild = delNode.rchild;
+            else
+                parent.rchild = delNode.rchild;
+            return true;
+        }
+
+        // 3. 删除节点有左孩子和右孩子
+        // a. 找寻右子树的最小子节点（待删除节点的直接中序后继），记录该节点值用户替换待删除节点值
+        // b. 删除中序后继节点，该中序后继一定满足 2.
+        // 注意最小子节点就是待删除节点的右孩子的情况
+        if (delNode.lchild != null && delNode.rchild != null) {
+            BinarySortNode minNode = delNode.rchild;
+            // 查询中序直接后继
+            while (minNode.lchild != null)
+                minNode = minNode.lchild;
+            int temp = minNode.data;
+            BinarySortNode minParent = searchParent(delNode.rchild, minNode);
+            if (minParent == null)
+                // 最小子节点就是待删除节点的右孩子，根据中序遍历规则，此时最小子节点的左子树一定为空，这里删除最小子节点相当于2.2
+                delNode.rchild = minNode.rchild;
+            else
+                // 删除最小子节点，此时情况一定是属于 2.2 里else if分支
+                minParent.lchild = minNode.rchild;
+            delNode.data = temp;
+            return true;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * 删除节点
+     *
+     * @param value
+     * @return boolean
+     * @author tqyao
+     * @create 2022/11/11 16:22
+     */
+    public boolean delNode(int value) {
+        BinarySortNode delNode = searchNodeTraverse(value);
+        if (null == delNode)
+            return false;
+
+        // 1. 如果是待删除节点是叶子节点
+        BinarySortNode parent = searchParent(delNode);
+//        if (delNode.lchild == null && delNode.rchild == null) {
+//            if (null == parent)
+//                root = null;
+//            else if (parent.lchild != null && parent.lchild == delNode)
+//                parent.lchild = null;
+//            else
+//                parent.rchild = null;
+//            return true;
+//        }
+
+
+//        // 2.1 如果待删除节的左孩子存在且右孩子不存在
+//        if (delNode.lchild != null && delNode.rchild == null) {
+//            if (parent == null)
+//                root = delNode.lchild;
+//            else if (parent.rchild != null && parent.rchild == delNode)
+//                parent.rchild = delNode.lchild;
+//            else
+//                parent.lchild = delNode.lchild;
+//            return true;
+//        }
+//
+//        // 2.2 若果待删除节点的左孩子不存在且右孩子存在
+//        if (delNode.lchild == null && delNode.rchild != null) {
+//            if (parent == null)
+//                root = delNode.rchild;
+//            else if (parent.lchild != null && parent.lchild == delNode)
+//                parent.lchild = delNode.rchild;
+//            else
+//                parent.rchild = delNode.rchild;
+//        }
+
+        if (!delLeafNode(parent, delNode))
+            if (!delLonelyNode(parent, delNode))
+                /*
+                 * 3. 如果待删除节点的左或右孩子都存在
+                 * a. 用待删除节点的中序直接后继（或直接前驱）替换掉待删除节点
+                 * b. 此时中序直接后，且情况一定属于步骤1.继（前驱）成了待删除节点 或者2.
+                 * */
+                if (delNode.lchild != null && delNode.rchild != null) {
+                    // delNode中序遍历的第一个后继
+                    BinarySortNode firstSubInOrder = getSubOfInOrder(delNode.rchild);
+                    // 后继的父节点
+                    BinarySortNode subParent = searchParent(firstSubInOrder);
+                    // b.
+                    if (!delLeafNode(subParent, firstSubInOrder))
+                        delLonelyNode(subParent, firstSubInOrder);
+                    // a.
+                    if (delNode.lchild != null)
+                        firstSubInOrder.lchild = delNode.lchild;
+                    if (delNode.rchild != null)
+                        firstSubInOrder.rchild = delNode.rchild;
+                }
+
+        return true;
+    }
+
+    /**
+     * 获取指定节点的第一个中序遍历节点
+     *
+     * @param specifyNode
+     * @return fun.tqyao.tree.BinarySortTree_04.BinarySortNode
+     * @author tqyao
+     * @create 2022/11/10 23:19
+     */
+    private BinarySortNode getSubOfInOrder(BinarySortNode specifyNode) {
+        if (specifyNode == null)
+            return null;
+        while (specifyNode.lchild != null)
+            specifyNode = specifyNode.lchild;
+        return specifyNode;
+    }
+
+    /**
+     * 删除只有单一孩子的节点
+     *
+     * @param parent
+     * @param delNode
+     * @return boolean
+     * @author tqyao
+     * @create 2022/11/10 23:09
+     */
+    private boolean delLonelyNode(BinarySortNode parent, BinarySortNode delNode) {
+        // 2.1 如果待删除节的左孩子存在且右孩子不存在
+        if (delNode.lchild != null && delNode.rchild == null) {
+            if (parent == null)
+                root = delNode.lchild;
+            else if (parent.rchild != null && parent.rchild == delNode)
+                parent.rchild = delNode.lchild;
+            else
+                parent.lchild = delNode.lchild;
+            return true;
+        }
+
+        // 2.2 若果待删除节点的左孩子不存在且右孩子存在
+        if (delNode.lchild == null && delNode.rchild != null) {
+            if (parent == null)
+                root = delNode.rchild;
+            else if (parent.lchild != null && parent.lchild == delNode)
+                parent.lchild = delNode.rchild;
+            else
+                parent.rchild = delNode.rchild;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 删除叶子节点
+     *
+     * @param parent
+     * @param delNode
+     * @return boolean
+     * @author tqyao
+     * @create 2022/11/10 20:49
+     */
+    private boolean delLeafNode(BinarySortNode parent, BinarySortNode delNode) {
+        // 1. 如果是待删除节点是叶子节点
+        if (delNode.lchild == null && delNode.rchild == null) {
+            if (null == parent)
+                root = null;
+            else if (parent.lchild != null && parent.lchild == delNode)
+                parent.lchild = null;
+            else
+                parent.rchild = null;
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 在指定的排序二叉树中查找指定节点的父节点
+     *
+     * @param root
+     * @param specifyNode
+     * @return fun.tqyao.tree.BinarySortTree_04.BinarySortNode
+     * @author tqyao
+     * @create 2022/11/11 20:30
+     */
+    private BinarySortNode searchParent(BinarySortNode root, BinarySortNode specifyNode) {
+        if (root == null || specifyNode == null)
+            return null;
+        BinarySortNode parent = null;
+        while (root != null) {
+            if (root.data == specifyNode.data)
+                return parent;
+            // 记录当前节点作为下次迭代的父节点
+            parent = root;
+            // 指定节点值小于当前节点值，左子树迭代查找
+            if (root.data > specifyNode.data)
+                root = root.lchild;
+            else
+                root = root.rchild;
+        }
+        // 指定节点在排序树中不存在
+        return null;
+    }
+
+    /**
+     * 在跟节点中查找指定节点的父节点
+     * error : 如果指定节点不存在，会返回叶子节点作为父节点
+     *
+     * @param specifyNode
+     * @return fun.tqyao.tree.BinarySortTree_04.BinarySortNode
+     * @author tqyao
+     * @create 2022/11/10 18:13
+     */
+    @Deprecated
+    private BinarySortNode searchParent(BinarySortNode specifyNode) {
+        BinarySortNode cur, parent;
+        parent = null;
+        cur = root;
+        while (cur != null) {
+            parent = cur;
+            if (cur.data < specifyNode.data)
+                cur = cur.lchild;
+            else if (cur.data > specifyNode.data)
+                cur = cur.rchild;
+            else
+                break;
+        }
+        return parent;
+    }
+
+
+    /**
+     * 创建排序二叉树 by Traverse
+     *
+     * @param nodeVals
+     * @author tqyao
+     * @create 2022/11/10 08:55
+     */
+    public void createBSTByTraverse(int[] nodeVals) {
+        Arrays.stream(nodeVals).forEach(this::insertNodeTraverse);
+    }
+
+    /**
+     * 创建排序二叉树 by Traverse
+     *
+     * @param nodeVals
+     * @author tqyao
+     * @create 2022/11/10 08:55
+     */
+    public void createBSTByRecursion(int[] nodeVals) {
+//        for (int i : nodeVals)
+//            insertNodeRecursion(root, i);
+        Arrays.stream(nodeVals).forEach(vals -> root = insertNodeRecursion(root, vals));
+    }
+
 
     /**
      * 插入节点（遍历）
@@ -69,7 +426,7 @@ public class BinarySortTree_04 {
             return new BinarySortNode(value);
         // 插入值与排序二叉树节点值相等，返回空，不进行插入
         if (value == cur.data)
-            return null;
+            return cur;
         //插入值小于当前节点值，插入到左子树，否则插入到右子树。
         if (value < cur.data)
             cur.lchild = insertNodeRecursion(cur.lchild, value);
@@ -141,6 +498,27 @@ public class BinarySortTree_04 {
             this.data = data;
             this.lchild = lchild;
             this.rchild = rchild;
+        }
+
+//        @Override
+//        public String toString() {
+//            return "BinarySortNode{" +
+//                    "data=" + data +
+//                    ", lchild.data=" + lchild != null ? String.valueOf(lchild.data) : "0" +
+//                    ", rchild.data=" + rchild != null ? String.valueOf(rchild.data) : "0" +
+//                    '}';
+//        }
+
+        @Override
+        public String toString() {
+            Integer ldata, rdata;
+            ldata = lchild != null ? lchild.data : 0;
+            rdata = rchild != null ? rchild.data : 0;
+            return "BinarySortNode{" +
+                    "data=" + data +
+                    ", lchild.data=" + ldata +
+                    ", rchild.data=" + rdata +
+                    '}';
         }
     }
 }
---
+++ data-structures-demo/src/test/java/fun/tqyao/tree/BinarySortTree_04Test.java
@@ -0,0 +1,93 @@
+package fun.tqyao.tree;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+public class BinarySortTree_04Test {
+
+    @Test
+    public void testCompareTo() {
+        System.out.println("A".compareTo("b"));
+        System.out.println("A".compareTo("A"));
+        Object o = "asda";
+    }
+
+    static BinarySortTree_04 bstree;
+
+
+    @BeforeClass
+    public static void beforeClass() {
+//        int[] nodeVals = Arrays.stream("1,2,3,4,5,6".split(",")).mapToInt(Integer::parseInt).toArray();
+//        int[] nodeVals = Arrays.stream("53,17,78,9,45,65,78,87,23".split(",")).mapToInt(Integer::parseInt).toArray();
+        int[] nodeVals = Arrays.stream("53,17,78,9,45,65,94,23,81,88".split(",")).mapToInt(Integer::parseInt).toArray();
+        System.out.printf("nodevals = %s\n", Arrays.toString(nodeVals));
+        System.out.printf("nodeVals.length = %d\n", nodeVals.length);
+
+        bstree = new BinarySortTree_04();
+        bstree.createBSTByRecursion(nodeVals);
+        List<Integer> orderList = bstree.inOrderTraverse();
+        System.out.println(orderList.toString());
+        System.out.printf("orderList size = %d\n", orderList.size());
+
+//        System.out.println(">>>>>>>>>>>>>>>");
+//        BinarySortTree_04 bstree2 = new BinarySortTree_04();
+//        bstree2.createBSTByTraverse(nodeVals);
+//        List<Integer> orderList2 = bstree2.inOrderTraverse();
+//        System.out.println(orderList2.toString());
+//        System.out.printf("orderList2 size = %d\n",orderList2.size());
+    }
+
+    @Test
+    public void testRemove() {
+        System.out.println("remove >>>>>");
+//        bstree.remove(87);
+//        System.out.println(bstree.inOrderTraverse());
+
+        bstree.remove(78);
+        System.out.println(bstree.inOrderTraverse());
+        Optional.ofNullable(bstree.searchNodeTraverse(78)).ifPresent(System.out::println);
+        Optional.ofNullable(bstree.searchNodeTraverse(81)).ifPresent(System.out::println);
+
+        System.out.println("//////////////////");
+        Optional.ofNullable(bstree.searchNodeTraverse(94))
+                .map(node -> node.rchild).ifPresent(System.out::println);
+
+        System.out.println("||||||||||||||||||||");
+        Optional.ofNullable(bstree.searchNodeTraverse(94))
+                .map(node -> node.lchild).ifPresent(System.out::println);
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+
+    }
+
+    @Test
+    public void testSearchNode() {
+        // 左右孩子存在
+        System.out.println(bstree.searchNodeTraverse(53));
+        System.out.println(bstree.searchNodeRecursion(bstree.root, 53));
+
+        // 右孩子空
+        System.out.println(bstree.searchNodeTraverse(45));
+        System.out.println(bstree.searchNodeRecursion(bstree.root, 45));
+
+        // 不存在的节点值
+        System.out.println("====");
+        Optional.ofNullable(bstree.searchNodeTraverse(111)).ifPresent(System.out::println);
+        Optional.ofNullable(bstree.searchNodeRecursion(bstree.root, 111)).ifPresent(System.out::println);
+        System.out.println("====");
+    }
+
+    @Test
+    public void testInOrder() {
+        System.out.println(bstree.inOrderTraverse().toString());
+        System.out.println(bstree.inOrderRecursion(bstree.root, null).toString());
+    }
+
+
+}
